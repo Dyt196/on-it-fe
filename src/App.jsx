f17795326b7a5c1e8effbec638b5e94cc323bd58
Dyt196 <m.ikhmalhidayat@gmail.com>
2025-10-03 15:42:09 +0800

    Update Add Button CSS

--- src/App.jsx
+++ src/App.jsx
@@ -83,7 +83,7 @@ function App() {
         <div className="flex w-full justify-center p-6">
           <button
             onClick={() => processCart()}
-            className={`${selectedItem.length === 0 || loadPack ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500"} text-white px-4 py-2 rounded`}
+            className={`${selectedItem.length === 0 || loadPack ? "bg-gray-400 cursor-not-allowed" : "bg-blue-500"} text-white px-4 py-2 rounded customButton`}
             disabled={selectedItem.length === 0 || loadPack}
 
           >
@@ -125,7 +125,7 @@ function App() {
             <div className="flex justify-end">
               <button
                 onClick={() => setDialog(false)}
-                className="px-4 py-2 bg-gray-300 rounded"
+                className="px-4 py-2 bg-blue-300 rounded"
               >
                 Close
               </button>
